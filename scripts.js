2ce8e1d62da7cee8c7aafd08b1a879ff82ba2f0e
bakaldina <e.bakaldina@gmail.com>
2018-04-13 19:30:14 +0300

    new

--- scripts.js
+++ scripts.js
@@ -37,20 +37,31 @@ jQuery.validator.addMethod("checkMask", function(value, element) {
   $('.js-phone').mask("+7(999)999-9999", {
     autoclear: false
   });
-  $("form").submit(function(){
-    var openbutton = document.querySelector('.js-OpenModal');
-    var modalwindow = document.querySelector('.modals');
-    var closebutton = document.querySelector('.modals .close');
-    var modalsOverley = document.querySelector('.modals__overlay');
-    modalwindow.classList.add("show");
-    var closebutton = document.querySelector('.modals .close');
-    var modalsOverley = document.querySelector('.modals__overlay');
-    modalsOverley.onclick = function() {
-        modalwindow.classList.remove("show");
+  $("form").submit(function(e){
+    e.preventDefault();
+    var phone = $('.js-phone').val().replace(/\D/g,'');
+    if (phone.length !== 11) {
+      return;
     }
-    closebutton.onclick = function() {
-        modalwindow.classList.remove("show");
-    }
+
+    var tokenName = $("meta[name=csrf-token-name]").prop("content");
+    var token = $("meta[name=csrf-token]").prop("content");
+
+    $.get("/", {phone: phone}, function(response) {
+        var openbutton = document.querySelector('.js-OpenModal');
+        var modalwindow = document.querySelector('.modals');
+        var closebutton = document.querySelector('.modals .close');
+        var modalsOverley = document.querySelector('.modals__overlay');
+        modalwindow.classList.add("show");
+        var closebutton = document.querySelector('.modals .close');
+        var modalsOverley = document.querySelector('.modals__overlay');
+        modalsOverley.onclick = function() {
+            modalwindow.classList.remove("show");
+        }
+        closebutton.onclick = function() {
+            modalwindow.classList.remove("show");
+        }
+    });
 });
   
 // // var openbutton = document.querySelector('.js-OpenModal');
