a92ed6da25ed92bee75f99c8e891733739a2d9db
bakaldina <e.bakaldina@gmail.com>
2018-04-13 15:55:32 +0300

    add_submit

--- scripts.js
+++ scripts.js
@@ -1,41 +1,70 @@
-var phoneInput = document.querySelector('.phone')
-phoneInput.addEventListener('keydown', function(event) {
-   if( !(event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Backspace' || event.key == 'Tab')) { event.preventDefault() }
-    var mask = '+7 (111) 111-11-11'; // Задаем маску
+// var phoneInput = document.querySelector('.phone')
+// phoneInput.addEventListener('keydown', function(event) {
+//    if( !(event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Backspace' || event.key == 'Tab')) { event.preventDefault() }
+//     var mask = '+7 (111) 111-11-11'; // Задаем маску
  
-    if (/[0-9\+\ \-\(\)]/.test(event.key)) {
-        // Здесь начинаем сравнивать this.value и mask
-        // к примеру опять же
-        var currentString = this.value;
-        var currentLength = currentString.length;
-        if (/[0-9]/.test(event.key)) {
-            if (mask[currentLength] == '1') {
-                this.value = currentString + event.key;
-            } else {
-                for (var i=currentLength; i<mask.length; i++) {
-                if (mask[i] == '1') {
-                    this.value = currentString + event.key;
-                    break;
-                }
-                currentString += mask[i];
-                }
-            }
-        }
-    } 
-});
+//     if (/[0-9\+\ \-\(\)]/.test(event.key)) {
+//         // Здесь начинаем сравнивать this.value и mask
+//         // к примеру опять же
+//         var currentString = this.value;
+//         var currentLength = currentString.length;
+//         if (/[0-9]/.test(event.key)) {
+//             if (mask[currentLength] == '1') {
+//                 this.value = currentString + event.key;
+//             } else {
+//                 for (var i=currentLength; i<mask.length; i++) {
+//                 if (mask[i] == '1') {
+//                     this.value = currentString + event.key;
+//                     break;
+//                 }
+//                 currentString += mask[i];
+//                 }
+//             }
+//         }
+//     } 
+// });
 
-var openbutton = document.querySelector('.js-OpenModal');
-var modalwindow = document.querySelector('.modals');
-var closebutton = document.querySelector('.modals .close');
-var modalsOverley = document.querySelector('.modals__overlay');
-
-openbutton.onclick = function() {
+jQuery.validator.addMethod("checkMask", function(value, element) {
+    return /\+\d{1}\(\d{3}\)\d{3}-\d{4}/g.test(value);
+  });
+  
+  $('form').validate();
+  $.validator.addClassRules({
+    'js-phone': {
+      checkMask: true
+    }
+  });
+  $('.js-phone').mask("+7(999)999-9999", {
+    autoclear: false
+  });
+  $("form").submit(function(){
+    var openbutton = document.querySelector('.js-OpenModal');
+    var modalwindow = document.querySelector('.modals');
+    var closebutton = document.querySelector('.modals .close');
+    var modalsOverley = document.querySelector('.modals__overlay');
     modalwindow.classList.add("show");
-}
+    var closebutton = document.querySelector('.modals .close');
+    var modalsOverley = document.querySelector('.modals__overlay');
+    modalsOverley.onclick = function() {
+        modalwindow.classList.remove("show");
+    }
+    closebutton.onclick = function() {
+        modalwindow.classList.remove("show");
+    }
+});
+  
+// // var openbutton = document.querySelector('.js-OpenModal');
+// // var modalwindow = document.querySelector('.modals');
+// var closebutton = document.querySelector('.modals .close');
+// var modalsOverley = document.querySelector('.modals__overlay');
 
-modalsOverley.onclick = function() {
-    modalwindow.classList.remove("show");
-}
-closebutton.onclick = function() {
-    modalwindow.classList.remove("show");
-}
+// // openbutton.onclick = function() {
+// //     modalwindow.classList.add("show");
+// // }
+
+// modalsOverley.onclick = function() {
+//     modalwindow.classList.remove("show");
+// }
+// closebutton.onclick = function() {
+//     modalwindow.classList.remove("show");
+// }
